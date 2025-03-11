c650b4210a9a79c3000181a910c1ee9358f5de9b
gusmendez3249 <gustavoangelc2005@email.com>
2025-03-11 11:37:05 -0600

    Codigo corregido 11/03/2025 clase torres

---
+++ SIGE/src/app/admin_empleado/registrar-empleado/registrar-empleado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-empleado',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrar-empleado.component.html',
+  styleUrl: './registrar-empleado.component.css'
+})
+export class RegistrarEmpleadoComponent {
+
+}
