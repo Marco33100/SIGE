4feeccad058dd89cdb52a05d0292826489c227ed
Marco <martinezramirezm82@gmail.com>
2025-03-16 13:11:17 -0600

    añadie el interface, lo borre por error

--- SIGE/src/services/actividad.service.ts
+++ SIGE/src/services/actividad.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Actividad } from '../app/interfaces/cursosActividades.interface';
+import { Actividad } from '../app/interface/cursosActividades.interface';
 
 @Injectable({
   providedIn: 'root'
