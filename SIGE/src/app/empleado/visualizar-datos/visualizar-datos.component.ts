732bd430a8d7aa94f61a17e671fda09cb0f0b95e
gusmendez3249 <gustavoangelc2005@email.com>
2025-03-17 00:55:23 -0600

    Domingo

---
+++ SIGE/src/app/empleado/visualizar-datos/visualizar-datos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizar-datos',
+  standalone: true,
+  imports: [],
+  templateUrl: './visualizar-datos.component.html',
+  styleUrl: './visualizar-datos.component.css'
+})
+export class VisualizarDatosComponent {
+
+}
